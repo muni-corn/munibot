87c85d5516832cba41e70a65a309337e61d84f46
municorn <municorn@musicaloft.com>
2025-07-19 22:49:23 -0600

    refactor: migrate from flake-utils to flake-parts
    Replace flake-utils with flake-parts for better flake structure and
    maintainability. Update input references and restructure outputs to
    use flake-parts' perSystem pattern.

--- flake.nix
+++ flake.nix
@@ -6,140 +6,146 @@
     crane.url = "github:ipetkov/crane";
     fenix.url = "github:nix-community/fenix";
     treefmt-nix.url = "github:numtide/treefmt-nix";
-    utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    {
-      self,
-      nixpkgs,
-      crane,
-      fenix,
-      treefmt-nix,
-      utils,
-    }:
-    utils.lib.eachDefaultSystem (
-      system:
-      let
-        name = "munibot";
-        pkgs = nixpkgs.legacyPackages.${system};
-        lib = pkgs.lib;
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
 
-        # treefmt formatting
-        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+      perSystem =
+        {
+          config,
+          self',
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          name = "munibot";
+          lib = pkgs.lib;
 
-        # make rust toolchain
-        toolchain =
-          with fenix.packages.${system};
-          combine [
-            complete.rust-src
-            complete.rustc-codegen-cranelift-preview
-            default.cargo
-            default.clippy
-            default.rustfmt
-            rust-analyzer
-            targets.wasm32-unknown-unknown.latest.rust-std
-          ];
+          # treefmt formatting
+          treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
 
-        # make build library
-        craneLib = (crane.mkLib pkgs).overrideToolchain toolchain;
+          # make rust toolchain
+          toolchain =
+            with inputs.fenix.packages.${system};
+            combine [
+              complete.rust-src
+              complete.rustc-codegen-cranelift-preview
+              default.cargo
+              default.clippy
+              default.rustfmt
+              rust-analyzer
+              targets.wasm32-unknown-unknown.latest.rust-std
+            ];
 
-        # build artifacts
-        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+          # make build library
+          craneLib = (inputs.crane.mkLib pkgs).overrideToolchain toolchain;
 
-        # establish commonly used arguments
-        commonArgs = {
-          src = lib.cleanSourceWith {
-            src = self;
-            filter =
-              path: type:
-              (lib.hasInfix "/assets/" path)
-              || (lib.hasInfix "/style/" path)
-              || (lib.hasSuffix "tailwind.config.js" path)
-              || (craneLib.filterCargoSources path type);
-          };
-          strictDeps = true;
-          stdenv = p: p.stdenvAdapters.useMoldLinker p.stdenv;
+          # build artifacts
+          cargoArtifacts = craneLib.buildDepsOnly commonArgs;
 
-          inherit nativeBuildInputs buildInputs cargoArtifacts;
-        };
-
-        nativeBuildInputs = with pkgs; [
-          clang
-          glibc
-          leptosfmt
-          pkg-config
-          trunk
-        ];
-        buildInputs = with pkgs; [ libressl_4_0 ];
-
-        munibot = craneLib.buildPackage (
-          commonArgs
-          // {
-            LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
-          }
-        );
-      in
-      {
-        # `nix build`
-        packages.default = munibot;
-
-        # `nix run`
-        apps.default = utils.lib.mkApp {
-          name = name;
-          drv = self.packages."${system}".default;
-          exePath = "/bin/${name}";
-        };
-
-        # `nix flake check`
-        checks = {
-          inherit munibot;
-          clippy = craneLib.cargoClippy (
-            commonArgs // { cargoClippyExtraArgs = "--all-targets --all-features"; }
-          );
-          formatting = treefmtEval.config.build.check self;
-        };
-
-        # `nix develop`
-        devShells.default =
-          let
-            moldDevShell = craneLib.devShell.override {
-              mkShell = pkgs.mkShell.override {
-                stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv;
-              };
+          # establish commonly used arguments
+          commonArgs = {
+            src = lib.cleanSourceWith {
+              src = inputs.self;
+              filter =
+                path: type:
+                (lib.hasInfix "/assets/" path)
+                || (lib.hasInfix "/style/" path)
+                || (lib.hasSuffix "tailwind.config.js" path)
+                || (craneLib.filterCargoSources path type);
             };
-          in
-          moldDevShell {
-            checks = self.checks.${system};
+            strictDeps = true;
+            stdenv = p: p.stdenvAdapters.useMoldLinker p.stdenv;
 
-            packages =
-              buildInputs
-              ++ (with pkgs; [
-                leptosfmt
-                cargo-edit
-                cargo-outdated
-                cargo-release
-                cargo-watch
-                flyctl
-                cargo-machete
-              ]);
-
-            LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-            RUST_LOG = "error,munibot=debug";
-            LEPTOS_TAILWIND_VERSION = "v3.4.14";
-            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+            inherit nativeBuildInputs buildInputs cargoArtifacts;
           };
 
-        # `nix fmt`
-        formatter = treefmtEval.config.build.wrapper;
-      }
-    )
-    // {
-      overlays.default = final: prev: {
-        munibot = self.packages.${prev.system}.default;
-      };
+          nativeBuildInputs = with pkgs; [
+            clang
+            glibc
+            leptosfmt
+            pkg-config
+            trunk
+          ];
+          buildInputs = with pkgs; [ libressl_4_0 ];
 
-      nixosModules.default = import ./nix/nixos.nix self;
+          munibot = craneLib.buildPackage (
+            commonArgs
+            // {
+              LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+              LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+            }
+          );
+        in
+        {
+          # `nix build`
+          packages.default = munibot;
+
+          # `nix run`
+          apps.default = {
+            type = "app";
+            program = "${self'.packages.default}/bin/${name}";
+          };
+
+          # `nix flake check`
+          checks = {
+            inherit munibot;
+            clippy = craneLib.cargoClippy (
+              commonArgs // { cargoClippyExtraArgs = "--all-targets --all-features"; }
+            );
+            formatting = treefmtEval.config.build.check inputs.self;
+          };
+
+          # `nix develop`
+          devShells.default =
+            let
+              moldDevShell = craneLib.devShell.override {
+                mkShell = pkgs.mkShell.override {
+                  stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv;
+                };
+              };
+            in
+            moldDevShell {
+              checks = config.checks;
+
+              packages =
+                buildInputs
+                ++ (with pkgs; [
+                  leptosfmt
+                  cargo-edit
+                  cargo-outdated
+                  cargo-release
+                  cargo-watch
+                  flyctl
+                  cargo-machete
+                ]);
+
+              LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+              RUST_LOG = "error,munibot=debug";
+              LEPTOS_TAILWIND_VERSION = "v3.4.14";
+              LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+            };
+
+          # `nix fmt`
+          formatter = treefmtEval.config.build.wrapper;
+        };
+
+      flake = {
+        overlays.default = final: prev: {
+          munibot = inputs.self.packages.${prev.system}.default;
+        };
+
+        nixosModules.default = import ./nix/nixos.nix inputs.self;
+      };
     };
 }
