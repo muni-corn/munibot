714bb9a71e5ed0bcb6e46eae450fd8e595c2da40
municorn <municorn@musicaloft.com>
2026-03-25 09:44:16 -0600

    refactor(db)!: remove surrealdb `DbItem` trait and dependencies
    - remove `DbItem` trait as it's no longer needed for MySQL-based approach
    - remove surrealdb-related imports and dependencies
    - simplify db module to focus on diesel async connection pooling
    - consolidate database abstraction to single MySQL backend
    
    BREAKING CHANGE: `DbItem` trait has been removed; database operations must use diesel async directly

--- src/db.rs
+++ src/db.rs
@@ -1,10 +1,7 @@
-use async_trait::async_trait;
 use diesel_async::{
     AsyncMysqlConnection,
     pooled_connection::{AsyncDieselConnectionManager, bb8::Pool},
 };
-use serde::{Serialize, de::DeserializeOwned};
-use surrealdb::{Connection, RecordIdKey, Surreal, opt::IntoResource};
 
 pub mod migration;
 pub mod models;
@@ -25,35 +22,4 @@ pub async fn establish_pool() -> Result<DbPool, Box<dyn std::error::Error + Send
     let config = AsyncDieselConnectionManager::<AsyncMysqlConnection>::new(&database_url);
     let pool = Pool::builder().build(config).await?;
     Ok(pool)
-}
-
-#[async_trait]
-pub trait DbItem<C: Connection>: Serialize + DeserializeOwned {
-    const NAME: &'static str;
-    type Id: Into<RecordIdKey>;
-    type GetQuery;
-    type UpsertContent: Serialize + Send + 'static;
-
-    fn get_id(&self) -> Self::Id;
-
-    fn as_into_resource(&self) -> impl IntoResource<Option<Self>> {
-        (Self::NAME, self.get_id())
-    }
-
-    async fn get_from_db(
-        db: &Surreal<C>,
-        query: Self::GetQuery,
-    ) -> Result<Option<Self>, surrealdb::Error>;
-
-    async fn upsert_in_db<'a>(
-        &self,
-        db: &'a Surreal<C>,
-        content: Self::UpsertContent,
-    ) -> Result<Option<Self>, surrealdb::Error> {
-        db.upsert(self.as_into_resource()).content(content).await
-    }
-
-    async fn delete_from_db(&self, db: &Surreal<C>) -> Result<Option<Self>, surrealdb::Error> {
-        db.delete(self.as_into_resource()).await
-    }
 }
