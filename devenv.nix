b387017064f60daa0341e53da8b406e75a781d8c
municorn <municorn@musicaloft.com>
2026-07-11 16:00:57 -0600

    chore(devenv): add new crates to RUST_LOG filter

--- devenv.nix
+++ devenv.nix
@@ -37,7 +37,7 @@ in
   '';
 
   env = {
-    RUST_LOG = "error,munibot=debug,munibot_core=debug,munibot_discord=debug,munibot_twitch=debug";
+    RUST_LOG = "error,munibot=debug,munibot_api=debug,munibot_core=debug,munibot_discord=debug,munibot_gui=debug,munibot_twitch=debug";
     LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
     # matches the redirect URI registered with discord for local development
     PORT = 8080;
